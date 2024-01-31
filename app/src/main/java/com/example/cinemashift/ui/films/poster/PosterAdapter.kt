91464c6344a4a3a8f5437792237661fea9f719ea
40rt <dfl1@tpu.ru>
2024-01-31 20:44:48 +0300

    + новые иконки в bottom_nav_menu

--- app/src/main/java/com/example/cinemashift/ui/films/poster/PosterAdapter.kt
+++ app/src/main/java/com/example/cinemashift/ui/films/poster/PosterAdapter.kt
@@ -3,6 +3,8 @@ package com.example.cinemashift.ui.films.poster
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.cinemashift.R
 import com.example.cinemashift.data.Film
 import com.example.cinemashift.databinding.FilmPosterItemBinding
 
@@ -36,7 +38,15 @@ class PosterViewHolder(private val binding: FilmPosterItemBinding) :
             filmSubtitle.text = film.genres.toString()
             filmRatingValue.text = film.userRatings.imdb + film.userRatings.kinopoisk
 //            filmImg = //TODO GLIDE???
-        //TODO stringFormat
+//            Glide.with(this)
+//                .load(film.img) // image url
+//                .placeholder(R.drawable.placeholder) // any placeholder to load at start
+//                .error(R.drawable.imagenotfound)  // any image in case of error
+//                .override(200, 200) // resizing
+//                .centerCrop()
+//                .into(binding.filmImg)
+//            Glide.with(context).load(film.img).into(filmImg)
+            //TODO stringFormat
         }
 
         itemView.setOnClickListener {
